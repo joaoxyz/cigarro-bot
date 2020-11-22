226628efe350248b9cc4257273df19f6606f0896
Joao Pedro Reis <jpreis808@gmail.com>
2020-11-22 03:54:34 -0300

    Read quotes from external file

--- bot.py
+++ bot.py
@@ -8,29 +8,19 @@ from random import choice
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-quotesList = [
-             'Quer ganhar um caminhão de CIGARRO? Envie CIGARRO para (69) 4002-8922 para concorrer a um caminhão de CIGARRO!',
-             'A estética do cigarro é daora pena q da cancer',
-             'Ô gente boa\nÔ gente boa\nEi\nTem cigarro aí?\nCigarro\nTem cigarro aí?',
-             'Pois eu gosto muito de cigarro e vô fuma bastante cigarro hmmm hmmm cigarro hmmm',
-             'Quem puder me ceder um cigarro neste momento difícil eu agradeço',
-             'MUSICAS PARA FUMAR CIGARRO',
-             'o cigarro une as pessoas',
-             'hoje não almocei estou a base de pão água e cigarro qualquer hora eu morro',
-             'filho da puta, deu dois socao na minha cabeça só pq acendi um cigarro dentro do quarto pnc',
-             'MINHA MÃE MEXEU NAS MINHAS COISAS E ENCONTROU CIGARRO',
-             'Vontade de comprar cigarro',
-             'Quer um cigarro amigo?',
-             'me segurando p n ir comprar cigarro pq n posso fumar mais'
-]
+file = open("quotes.txt")
+quotesList = file.readlines()
+file.close()
+
 bot = commands.Bot(command_prefix = '!')
 # bot = discord.Client()
 
 @bot.event
 async def on_ready():
+    await bot.change_presence(activity = discord.Game('!cigarro')) # set custom status
     print(f'{bot.user} has connected to Discord!')
 
-@bot.command(name='cigarro')
+@bot.command(name = 'cigarro')
 async def quote(ctx):
     await ctx.send(choice(quotesList))
 
